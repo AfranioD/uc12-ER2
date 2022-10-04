895ff28c61885eb78466bbad0a70d0feb7bd30f8
AFRANIO DIMAS DA SILVA <86030954+AfranioD@users.noreply.github.com>
2022-10-04 16:25:56 -0300

    Add files via upload
    Adicionado arquivos criados no ER2 da SA2 - Uc12

---
+++ CadastroPessoaUc12/Program.cs
@@ -0,0 +1,9 @@
+﻿using CadastroPessoaUc12.Classes;
+
+PessoaFisica novaPf =new PessoaFisica();
+
+novaPf.Nome = "Afranio";
+
+Console.WriteLine(novaPf.Nome);
+
+Console.WriteLine($"Nome: {novaPf.Nome}");
